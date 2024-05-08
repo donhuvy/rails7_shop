0e96ac8c35e1adcf4fca3cffdd943c9c4ff74f7e
Vy Do <donhuvy@outlook.com>
2024-05-08 17:54:59 +0700

    add devise. errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :appusers
   get 'home/index'
   resources :brands
   resources :products
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_08_094154) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_08_104243) do
+  create_table "appusers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_appusers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_appusers_on_reset_password_token", unique: true
+  end
+
   create_table "brands", force: :cascade do |t|
     t.string "brand_name"
     t.string "company_name"
